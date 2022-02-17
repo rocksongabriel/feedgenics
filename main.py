ec059d6dba3858db676b5004bee902bba8c33c69
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-17 00:11:58 +0000

    Refactor the code, send the email every 5 minutes, and change the subject of the email

--- main.py
+++ main.py
@@ -74,12 +74,12 @@ def set_up_smtp_server():
     return session
 
 
-def construct_email_message(from_address, to_address, message):
+def construct_email_message(from_address, to_address, subject, message):
     msg = MIMEMultipart()  # create a message
     # Setup the parameters of the message
     msg["From"] = from_address
     msg["To"] = to_address
-    msg["Subject"] = f"Most Recent Upwork Job Entry Matching Your Skills"
+    msg["Subject"] = subject
     msg.attach(MIMEText(message, "plain"))
 
     return msg
@@ -101,12 +101,13 @@ def send_email(entry):
         DATE_PUBLISHED=entry.published,
     )
     
-    msg = construct_email_message(from_address, to_address, message)
+    subject = f"Most Recent Upwork Job Entry Matching Your Skills - {entry.title}"
+    msg = construct_email_message(from_address, to_address, subject, message)
     session.send_message(msg)
     print("Email sent ------------------------------")
 
 
-@repeat(every(1).minutes)
+@repeat(every(5).minutes)
 def check_most_recent_feed():
     most_recent__url = "https://www.upwork.com/ab/feed/topics/rss?securityToken=2f5d5ccd155d0ed6da9d7645a7a421fd418dfbd3d61b4c516bf0367377c57f619ebc1852950b1f1d78e111c66f552da4eb3297b0beefa4d1c9b9d4b97883aac8&userUid=1492101406951632896&orgUid=1492101406951632897&topic=most-recent"
     rss = feedparser.parse(most_recent__url)
