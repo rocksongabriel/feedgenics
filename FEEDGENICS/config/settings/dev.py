c1e9932ff1719fe411bd7f9dfb5f2020203ef058
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-17 02:02:37 +0000

    Set the project up using multiple a multiple settingstructure

---
+++ FEEDGENICS/config/settings/dev.py
@@ -0,0 +1,10 @@
+from .base import *
+
+
+SECRET_KEY = "=00qz-2zh*e5!0@c&&b9$ro+u)dot^f84d*0^4fbc)qg5(#n)7"
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS += []
---
+++ FEEDGENICS/config/settings/prod.py
@@ -0,0 +1,9 @@
+from .base import *
+
+SECRET_KEY = "ha+h4@749l$!@$wf&hjw86^*@!483nf*v^=@r2p=s2-wk8oh5r"
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS += []
