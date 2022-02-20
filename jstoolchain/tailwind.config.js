3fe765fbef139263690fa089641261fc5c387d16
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-20 18:28:18 +0000

    Add custom colors and fonts

--- jstoolchain/tailwind.config.js
+++ jstoolchain/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["../templates/**/*.html", "../templates/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "almost-black": "#1D2327",
+        "cornflower-blue": "#3361CC",
+        "mustard-brown": "#CC9E33",
+      },
+      fontFamily: {
+        "open-sans": ["Open Sans", "sans-serif"],
+        "montserrat-alternatives": ["Montserrat Alternates", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
