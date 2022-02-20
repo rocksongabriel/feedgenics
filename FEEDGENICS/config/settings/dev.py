d46f8fd7c7912319c6e19b96b2235a42849ede59
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-20 20:36:43 +0000

    Install django debug toolbar

--- FEEDGENICS/config/settings/dev.py
+++ FEEDGENICS/config/settings/dev.py
@@ -7,11 +7,22 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-INSTALLED_APPS += []
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
 
 SHELL_PLUS = "ipython"
 
 # EMAIL CONFIGURATION FOR TESTING
 DEFAULT_FROM_EMAIL = "test@feedgenics.com"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+MIDDLEWARE.insert(
+    2,
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+)
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- FEEDGENICS/config/urls.py
+++ FEEDGENICS/config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
