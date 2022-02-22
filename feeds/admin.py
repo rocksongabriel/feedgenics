e1a2285a068952a9cb877b950f11604bb8eadcf7
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-22 20:24:59 +0000

    Register the models on the admin

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Feed, Entry
+
+
+@admin.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ["name", "created", "number_of_update_posts"]
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ["title", "published"]
