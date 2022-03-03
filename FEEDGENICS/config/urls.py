653e0efc07a0bd5abcc0c6961e658e98b21a7538
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-03-03 04:09:38 +0000

    Add the url conf of the dashboard

--- FEEDGENICS/config/urls.py
+++ FEEDGENICS/config/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("pages.urls", namespace="pages")),
+    path("dashboard/", include("users.urls", namespace="users")),
 ]
 
 if settings.DEBUG:
